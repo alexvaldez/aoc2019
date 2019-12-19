dd3ea545b48da3fc03fe38551441addca9b7eeb8
Alex Valdez <179569+alexvaldez@users.noreply.github.com>
2019-12-19 17:36:35 -0600

    day 10 part 2

--- src/aoc/day10.clj
+++ src/aoc/day10.clj
@@ -14,31 +14,62 @@
             []
             (range (count lines)))))
 
-(defn count-viz [field [x0 y0]]
-  (+ (count (reduce (fn [m [x y]]
-                      (update-in m
-                                 [(/ (- x x0) (- y y0))]
-                                 (fnil conj #{}) [x y]))
-                    {}
-                    (filter (fn [[x y]] (and (>= x x0) (> y y0))) field)))
-     (count (reduce (fn [m [x y]]
-                      (update-in m
-                                 [(/ (- y y0) (- x x0))]
-                                 (fnil conj #{}) [x y]))
-                    {}
-                    (filter (fn [[x y]] (and (< x x0) (>= y y0))) field)))
-     (count (reduce (fn [m [x y]]
-                      (update-in m
-                                 [(/ (- x x0) (- y y0))]
-                                 (fnil conj #{}) [x y]))
-                    {}
-                    (filter (fn [[x y]] (and (<= x x0) (< y y0))) field)))
-     (count (reduce (fn [m [x y]]
-                      (update-in m
-                                 [(/ (- y y0) (- x x0))]
-                                 (fnil conj #{}) [x y]))
-                    {}
-                    (filter (fn [[x y]] (and (> x x0) (<= y y0))) field)))))
+(defn calc-quadrants [field [x0 y0]]
+  [(reduce (fn [m [x y]]
+             (update-in m
+                        [(/ (- x x0) (- y0 y))]
+                        (fnil conj (sorted-set-by (fn [p1 p2] (> (second p1) (second p2)))))
+                        [x y]))
+           (sorted-map)
+           (filter (fn [[x y]] (and (>= x x0) (< y y0))) field))
+   (reduce (fn [m [x y]]
+             (update-in m
+                        [(/ (- y y0) (- x x0))]
+                        (fnil conj (sorted-set-by (fn [p1 p2] (< (first p1) (first p2)))))
+                        [x y]))
+           (sorted-map)
+           (filter (fn [[x y]] (and (> x x0) (>= y y0))) field))
+   (reduce (fn [m [x y]]
+             (update-in m
+                        [(/ (- x0 x) (- y y0))]
+                        (fnil conj (sorted-set-by (fn [p1 p2] (< (second p1) (second p2)))))
+                        [x y]))
+           (sorted-map)
+           (filter (fn [[x y]] (and (<= x x0) (> y y0))) field))
+   (reduce (fn [m [x y]]
+             (update-in m
+                        [(/ (- y0 y) (- x0 x))]
+                        (fnil conj (sorted-set-by (fn [p1 p2] (> (first p1) (first p2)))))
+                        [x y]))
+           (sorted-map)
+           (filter (fn [[x y]] (and (< x x0) (<= y y0))) field))])
+
+(defn count-viz [quadrants]
+  (apply + (map count quadrants)))
 
 (defn day10-1 [field]
-  (apply max (map #(count-viz field %) field)))
+  (apply max (map #(count-viz (calc-quadrants field %)) field)))
+
+(defn get-targets [q]
+  (if (empty? q)
+    q
+    (let [head (peek q)
+          body (pop q)
+          tail (rest head)]
+      (if (empty? tail)
+        body
+        (conj body tail)))))
+
+(defn day10-2 [field]
+  (let [[maximum quadrants] (reduce (fn [[mx q] p]
+                                      (let [quadrants (calc-quadrants field p)
+                                            cv (count-viz quadrants)]
+                                        (if (> cv mx)
+                                          [cv quadrants]
+                                          [mx q])))
+                                    [0 nil]
+                                    field)
+        queue (into clojure.lang.PersistentQueue/EMPTY (apply concat (map vals quadrants)))]
+    (nth (map (fn [q] (first (peek q))) (iterate get-targets queue)) 199)))
+
+
